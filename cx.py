9984da0f6d09f638c76b1dfcf149a85e63d0401a
git-sgg <git-sgg@users.noreply.github.com>
2026-09-11 15:53:35 +0800

    feat: cx version 命令 + status 显示版本 (1.0.3)

--- cx.py
+++ cx.py
@@ -14,7 +14,7 @@
 """
 import json, os, re, glob, sqlite3, subprocess, sys, shutil, time, urllib.request, urllib.error
 
-CX_VERSION = "1.0.2"
+CX_VERSION = "1.0.3"
 
 CODEX_HOME = os.environ.get("CODEX_HOME", os.path.expanduser("~/.codex"))
 CONFIG = os.path.join(CODEX_HOME, "config.toml")
